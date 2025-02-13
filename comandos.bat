7fd2fe6a30fc176d017bf5fdf805dcf4840f13f7
isabellamchd <maria.i.machado6@aluno.senai.br>
2025-02-13 15:59:46 -0300

    Add files via upload

---
+++ comandos.bat
@@ -0,0 +1,16 @@
+mkdir projeto
+pause
+cd projeto
+pause
+echo projeto > milho.txt
+pause
+cd ..
+pause
+rename projeto sorvete
+pause
+mkdir projeto2
+pause
+move sorvete\milho.txt projeto2\milho.txt
+pause
+rename "projeto2" "chocolate"
+pause
